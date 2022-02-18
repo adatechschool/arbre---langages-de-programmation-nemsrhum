e0426357eb3b0f2ed404386e73b82b0d175e317f
Cyril <lgcyriltt@gmail.com>
2022-02-18 16:12:01 +0100

    AJOUT ETAPE 6 OK AVEC BONUS SURPRISE

--- JEUX CYRIL/js/script.js
+++ JEUX CYRIL/js/script.js
@@ -3,6 +3,8 @@ let nb;
 let guessNB;
 let nbToGuess;
 let cpt=0;
+let minRange;
+let maxRange;
 
 function myNumber(givenNumber){
    let intNumber=parseInt(givenNumber);
@@ -66,7 +68,7 @@ function askANumber () {
    return nb;
 }*/
 
-// VERSION 4
+/*// VERSION ETAPE 3
 function askANumber1 () {
    guessNB=myNumber(prompt("Votre nombre entre 0 et 50 :",""))
    return guessNB;
@@ -76,9 +78,11 @@ function askANumber2 () {
    nb=myNumber(prompt("Votre nombre :",""))
    return nb;
 }
+*/
 
+/*// VERSION ETAPE 5
 function nbInRange (n) {
-   if (n<0 || n>50) {
+   if (n<minRange || n>maxRange) {
       console.log("nbInRange retourne False : " + n);
       return false;
    }
@@ -87,7 +91,20 @@ function nbInRange (n) {
       return true;
    };
 }
+*/
 
+// VERSION ETAPE 6
+function nbInRange (n) {
+   if (n<minRange || n>maxRange) {
+      console.log("nbInRange retourne False : " + n);
+      //alert ("Votre chiffre sort du range 0-50!");
+      return false;
+   }
+   else {
+      console.log("nbInRange retourne True : " + n); 
+      return true;
+   };
+}
 
 /*  // VERSION 2
 function gamePlay() {
@@ -103,7 +120,6 @@ function gamePlay() {
 
 // VERSION 5
 function compteur () {
-
    cpt++;
    return cpt;
 }
@@ -142,7 +158,7 @@ function gamePlay() {
       
    };*/
 
-// VERSION 5
+/*// VERSION ETAPE 5
 function gamePlay2() {
 
    nbToGuess= askANumber5("joueur1");
@@ -158,19 +174,26 @@ function gamePlay2() {
       document.getElementById("message").innerHTML="PLACE AU JOUEUR 2!"
    
    }
+}*/
 
+// VERSION ETAPE 6
+function gamePlay3() {
+   nbToGuess= askANumber5("joueur1");
+   console.log("NB TO GUESS : " + nbToGuess);
+   document.getElementById("joueur1").value="";
+   document.getElementById("message").innerHTML="PLACE AU JOUEUR 2!";
+}
 
-
-    // VERSION 2
+// VERSION 2
   /* while (!didIWin()) {
 
       askANumber();
    };*/
       //var test=document.nbToGuess.input.value;
    //console.log("Test : " + test);
-}
 
-/* // VERSION 5  : JOUEUR 2 */
+
+/* // VERSION 5  : JOUEUR 2 
 function didIWin(idJoueur) {
    var nbJX = document.getElementById(idJoueur).value;
    nb=myNumber(nbJX);
@@ -205,6 +228,59 @@ function didIWin(idJoueur) {
          //console.log(nb);
          document.getElementById("score").innerHTML = "<br><br>Nombre de tentative(s) : " + compteur ();
          return false
+      }
+      return false
+  }
+ 
+}*/
+
+/* // VERSION ETAPE 6  : JOUEUR 2 */
+function didIWin(idJoueur) {
+   var nbJX = document.getElementById(idJoueur).value;
+   nb=myNumber(nbJX);
+   //alert("Vous avez tapé : " + test);
+   //console.log("Vous avez tapé : " + nb);
+   
+   if (nb==nbToGuess)  { 
+      alert("YOU DID IT !!!","");
+      //console.log(nb);
+      document.getElementById("score").innerHTML = "<br><br>Nombre de tentative(s) : " + compteur ();
+      document.getElementById("message").innerHTML="BRAVO JOUEUR 2! Tu as droit à un bonus !"
+      document.getElementById("bonus").innerHTML="Click 'GO' to play: <button id='ok' onclick='myName()'>GO!</button>" ; // 1ere solution : mettre du html dans JS; 2e sol: separer html du JS mais appelle le DOM ci-dessous :
+      // document.getElementById("ok").setAttribute("style", "visibility:visible;");  // 2e solution : Pour rendre visible le bouton OK
+      display_image('img/rat.jfif', 
+      276, 
+      250, 
+      'JavaScriptImage');
+      
+      return true;
+   }
+
+   else { 
+      if  (!nbInRange(nb)) {
+         console.log("Pas dans range : " + nb);
+         //nbToGuess=askANumber5("joueur1");
+         //document.getElementById("message").innerHTML="Salut\! Entre un chiffre entre  0 < ? < 50"
+         document.getElementById("score").innerHTML = "<br><br>Nombre de tentative(s) : " + compteur ();
+      }      
+      else {
+
+         if (nb<nbToGuess) { 
+            myNumber(alert(`TROP PETIT ! Play again! Entre un chiffre entre ${minRange}<?<${maxRange} `,""));
+            minRange=nb;
+            console.log(`minRange = ${minRange}`);
+            document.getElementById("score").innerHTML = "<br><br>Nombre de tentative(s) : " + compteur ();
+            return false
+         }
+
+         else if (nb>nbToGuess) { 
+            myNumber(alert(`TROP GRAND ! Play again! Entre un chiffre entre ${minRange}<?<${maxRange} `,""));
+            //console.log(nb);
+            maxRange=nb;
+            console.log(`maxRange = ${maxRange}`);
+            document.getElementById("score").innerHTML = "<br><br>Nombre de tentative(s) : " + compteur ();
+            return false
+         }
       }
       return false
   }
